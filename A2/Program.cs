ed949d95a04bcece86332f9136e8f50d2e4a2a04
ArtemLevin <python_study_2022@mail.ru>
2022-12-02 11:31:46 +0300

    Fixed Task 2

--- A2/Program.cs
+++ A2/Program.cs
@@ -6,7 +6,6 @@ Console.WriteLine("Компьютер выбрал число: " + N);
 //int a11 = a1/10;
 int a11 = N/100;
 int a3 = N%10;
-int a2 = a1%10;
 int N1 = 10*a11 + a3;
 
 // Console.WriteLine("Вспомогательный расчет: " + a1);
